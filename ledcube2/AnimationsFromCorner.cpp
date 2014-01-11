bb0d1efb626ec9bc53280537f4a1562f3e0f7dd7
james <james@.(none)>
2014-01-11 12:22:15 +0000

    latest before consolidating functions

--- ledcube2/AnimationsFromCorner.cpp
+++ ledcube2/AnimationsFromCorner.cpp
@@ -29,7 +29,238 @@
 	}
 
 
+
+	switch(cube->ActiveCorner) {
+	case Corner_xyz:
+		x=y=z=0;
+		break;
+	case Corner_pxyz:
+		x=7;y=z=0;
+		break;
+	case Corner_xpyz:
+		x=z=0;y=7;
+		break;
+	case Corner_pxpyz:
+		x=y=7;z=0;
+		break;
+	case Corner_xypz:
+		x=y=0;z=7;
+		break;
+	case Corner_pxypz:
+		x=z=7;y=0;
+		break;
+	case Corner_xpypz:
+		x=0;y=z=7;
+		break;
+	case Corner_pxpypz:
+		x=y=z=7;
+		break;
+	}
+
+
 	*/
+
+void anim_CornerToPlaneDiagonalFill() {
+	char x, y, z;
+	char dx, dy, dz;
+	dx=dy=dz=0;
+	switch(cube->ActiveCorner) {
+	case Corner_xyz:
+		x=y=z=0;
+		switch(random(3)) {
+		case 0:
+			dx = 1;
+			dz = 1;
+			cube->ActivePlane = Plane_y;
+			break;
+		case 1:
+			dy = 1;
+			dz = 1;
+			cube->ActivePlane = Plane_x;
+			break;
+		case 2:
+			dx = 1;
+			dy = 1;
+			cube->ActivePlane = Plane_z;
+			break;
+		}
+		break;
+	case Corner_pxyz:
+		x=7;y=z=0;
+		switch(random(3)) {
+		case 0:
+			dx = -1;
+			dz = 1;
+			cube->ActivePlane = Plane_y;
+			break;
+		case 1:
+			dy = 1;
+			dz = 1;
+			cube->ActivePlane = Plane_px;
+			break;
+		case 2:
+			dz = 1;
+			dx = -1;
+			cube->ActivePlane = Plane_z;
+			break;
+		}
+		break;
+	case Corner_xpyz:
+		x=z=0;y=7;
+		switch(random(3)) {
+		case 0:
+			dx = 1;
+			dy = -1;
+			cube->ActivePlane = Plane_z;
+			break;
+		case 1:
+			dy = -1;
+			dz = 1;
+			cube->ActivePlane = Plane_x;
+			break;
+		case 2:
+			dz = 1;
+			dx = 1;
+			cube->ActivePlane = Plane_py;
+			break;
+		}
+		break;
+	case Corner_xypz:
+		x=y=0;z=7;
+		switch(random(3)) {
+		case 0:
+			dx = 1;
+			dy = 1;
+			cube->ActivePlane = Plane_pz;
+			break;
+		case 1:
+			dy = 1;
+			dz = -1;
+			cube->ActivePlane = Plane_x;
+			break;
+		case 2:
+			dz = -1;
+			dx = 1;
+			cube->ActivePlane = Plane_y;
+			break;
+		}
+		break;
+	case Corner_pxpyz:
+		x=y=7;z=0;
+		switch(random(3)) {
+		case 0:
+			dx = -1;
+			dz = 1;
+			cube->ActivePlane = Plane_py;
+			break;
+		case 1:
+			dy = -1;
+			dx = -1;
+			cube->ActivePlane = Plane_z;
+			break;
+		case 2:
+			dz = 1;
+			dy = -1;
+			cube->ActivePlane = Plane_px;
+			break;
+		}
+		break;
+	case Corner_pxypz:
+		x=z=7;y=0;
+		switch(random(3)) {
+		case 0:
+			dx = -1;
+			dy = 1;
+			cube->ActivePlane = Plane_pz;
+			break;
+		case 1:
+			dy = 1;
+			dz = -1;
+			cube->ActivePlane = Plane_px;
+			break;
+		case 2:
+			dz = -1;
+			dx = -1;
+			cube->ActivePlane = Plane_y;
+			break;
+		}
+		break;
+	case Corner_xpypz:
+		x=0;y=z=7;
+		switch(random(3)) {
+		case 0:
+			dx = 1;
+			dy = -1;
+			cube->ActivePlane = Plane_pz;
+			break;
+		case 1:
+			dy = -1;
+			dz = -1;
+			cube->ActivePlane = Plane_x;
+			break;
+		case 2:
+			dz = -1;
+			dx = 1;
+			cube->ActivePlane = Plane_py;
+			break;
+		}
+		break;
+	case Corner_pxpypz:
+		x=y=z=7;
+		switch(random(3)) {
+		case 0:
+			dx = -1;
+			dy = -1;
+			cube->ActivePlane = Plane_pz;
+			break;
+		case 1:
+			dy = -1;
+			dz = -1;
+			cube->ActivePlane = Plane_px;
+			break;
+		case 2:
+			dz = -1;
+			dx = -1;
+			cube->ActivePlane = Plane_py;
+			break;
+		}
+		break;
+	}
+
+	char cx, cy, cz;
+	for (char c = 0; c< 22;c++) {
+		cx = x;
+		do {
+			cy = y;
+
+			do {
+
+				cz = z;
+				do {
+
+
+					char xx, yy, zz;
+					xx = dx == -1 ? 7-cx : cx;
+					yy = dy == -1 ? 7-cy : cy;
+					zz = dz == -1 ? 7-cz : cz;
+					if (xx+yy+zz == c)
+						cube->SetLed(cx,cy,cz, true);
+					cz += dz;
+				} while(dz != 0 && abs(z-cz) != 8);
+				cy+=dy;
+			} while (dy != 0 && abs(y-cy) != 8);
+			cx += dx;
+		} while (dx != 0 && abs(x-cx) != 8);
+		delay(ANIMFAST);
+	}
+	cube->State = State_plane;
+
+
+	delay(1000);
+	cube->SetAll(false);
+	cube->State = State_off;
+
+}
 
 void anim_CornerToCornerPixelSimple() {
 	char x, y, z;
