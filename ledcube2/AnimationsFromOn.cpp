d537679c56a51664c3cd2dbad9c2016250db7082
james <>
2014-01-11 13:16:26 +0000

    after refactoring

--- ledcube2/AnimationsFromOn.cpp
+++ ledcube2/AnimationsFromOn.cpp
@@ -2,39 +2,8 @@
 
 void anim_OnToCornerDiagonal() {
 	CubeCorners c = (CubeCorners)random(8);
-	switch(c) {
-	case Corner_xyz:
-		anim_CornerToCorner(true, true, true, false, false);
-		cube->ActiveCorner = Corner_pxpypz;
-		break;
-	case Corner_pxyz:
-		anim_CornerToCorner(false, true, true, false, false);
-		cube->ActiveCorner = Corner_xpypz;
-		break;
-	case Corner_xpyz:
-		anim_CornerToCorner(true, false, true, false, false);
-		cube->ActiveCorner = Corner_pxypz;
-		break;
-	case Corner_xypz:
-		anim_CornerToCorner(true, true, false, false, false);
-		cube->ActiveCorner = Corner_pxpyz;
-		break;
-	case Corner_pxpyz:
-		anim_CornerToCorner(false, false, true, false, false);
-		cube->ActiveCorner = Corner_xypz;
-		break;
-	case Corner_pxypz:
-		anim_CornerToCorner(false, true, false, false, false);
-		cube->ActiveCorner = Corner_xpyz;
-		break;
-	case Corner_xpypz:
-		anim_CornerToCorner(true, false, false, false, false);
-		cube->ActiveCorner = Corner_pxyz;
-		break;
-	case Corner_pxpypz:
-		anim_CornerToCorner(false, false, false, false, false);
-		cube->ActiveCorner = Corner_xyz;
-		break;
-	}
+	CubeCorners target = getOppositeCornerCube(c);
+	anim_generalEnumerateAxis(c, target, false, false);
+	cube->ActiveCorner = target;
 	cube->State = State_corner;
 }
