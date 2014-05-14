ffe88babb712e56f9606dc32fb3edf99bf61ca63
james <>
2014-05-14 17:29:42 +0100

    tidy up

--- ledcube2/animations.h
+++ ledcube2/animations.h
@@ -23,20 +23,13 @@ extern AnimationFunction animFuncsPlane[];
 
 /* Animation Types
 
-Corner -> Corner
-Corner -> On
-On -> Corner
-On -> On
-Off -> Off
-Off -> Corner
-Corner -> Off
-On -> Off
-Off -> On
-Corner -> Plane
-Plane -> Corner
-Plane -> Plane
-Plane -> On
-On -> Plane
+		From:		Corner		Plane		Off			On
+To:
+Corner				3			1			1			1
+Plane				1			2						1
+Off					1						1
+On					1			1			1
+
 */
 
 void anim_nullAnimation();
@@ -71,24 +64,26 @@ extern const char NUMPLANEFUNCS;
 //general funcs
 void anim_SetCoordFromCorner(char *x, char* y, char *z, CubeCorners corner);
 void anim_generalEnumerateAxis(CubeCorners startCorner, CubeCorners endCorner, bool on, bool clear);
-void anim_ScrollPlanes();
 void anim_PlaneMove(Planes startPlane, bool on, bool clear, bool stopShort);
-//void anim_CornerToCorner(bool xPos, bool yPos, bool zPos, bool clear, bool on);
-void anim_RandomLineScroll();
 void anim_ScrollText(char* Text, unsigned char speed);
-void anim_Flash();
-void anim_CubeMove();
-
 void getPlanesForCorner(CubeCorners corner, Planes* planes);
 void getCornersForPlane(Planes plane, CubeCorners *corner);
 Planes getPlaneFromOppositeCorners(CubeCorners a, CubeCorners b);
-
 CubeCorners getOppositeCornerCube(CubeCorners corner);
 CubeCorners getOppositeCornerPlane(CubeCorners corner, Planes plane);
 Planes getOppositePlane(Planes plane);
+
 //get random opposite corner on plane from corner
 //get random adjacent corner on plane from corner
 
 unsigned char reverse(unsigned char b);
+
+/*
+void anim_ScrollPlanes();
+void anim_RandomLineScroll();
+void anim_Flash();
+void anim_CubeMove();
+*/
+
 
 #endif
